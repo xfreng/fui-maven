a885912e85e9afbde0833df12872e1ba516fb7d9
xfreng <xiong_jx@foxmail.com>
2017-08-09 14:59:54 +0800

    添加手机服务端

---
+++ common-utils/src/main/java/com/fui/common/CommonConstants.java
@@ -0,0 +1,20 @@
+package com.fui.common;
+
+/**
+ * @Title 常量表
+ * @Author sf.xiong on 2017/08/04.
+ */
+public interface CommonConstants {
+    /**
+     * 日期格式
+     */
+    String PARTTERN_YYYY_MM_DD = "yyyy-MM-dd";
+    String PARTTERN_YYYY_MM_DD_HH = "yyyy-MM-dd HH";
+    String PARTTERN_YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
+    String PARTTERN_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    String PARTTERN_YYYYMMDD = "yyyyMMdd";
+    String PARTTERN_YYYY_MM_DD_HH_MM_SS_SSS = "yyyyMMddHHmmssSSS";
+
+    //系统默认字符集
+    String DEFAULT_CHARACTER = "UTF-8";
+}
