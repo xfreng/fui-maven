eaae0d16acc196565738af9270025f7498b04adf
xfreng <xiong_jx@foxmail.com>
2017-12-25 15:44:21 +0800

    代码优化

--- admin-ms/src/main/java/com/fui/service/CalendarService.java
+++ admin-ms/src/main/java/com/fui/service/CalendarService.java
@@ -2,8 +2,6 @@ package com.fui.service;
 
 import com.fui.common.AbstractSuperService;
 import com.fui.dao.calendar.CalendarMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
